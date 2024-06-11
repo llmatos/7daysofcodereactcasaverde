74335ae4b99c50c90212b60276aa429ecdd13d34
Leandro Libório Matos <lmatos@SUPRG.local>
2024-06-11 17:31:32 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import NewsLetter from "./componentes/NewsLetter/NewsLetter";
+
+function App() {
+  return (
+    <>
+      <NewsLetter />
+    </>
+  );
+}
+
+export default App;
